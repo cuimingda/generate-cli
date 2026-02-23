cde83ef2413220d91c71b3154f8dd0fc3c8bd3f1
cuimingda <cuimingda@gmail.com>
2026-02-23 17:31:16 +0800

    添加主入口文件并更新根命令为 "gen"

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -1,10 +1,9 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/cuimingda/generate-cli/cmd"
+import cmd "github.com/cuimingda/generate-cli/cmd"
 
 func main() {
 	cmd.Execute()
